937b89668dbb1d4f5fc4e82052c9b879397d8bc0
Tom Hare <thare87@gmail.com>
2014-08-06 14:25:16 +0100

    Integrating Gulp web server and file include
    Moving away from BrowserSync to Gulp-webserver plugin with LiveReload.
    Also integrating Gulp-file-include for templates and partials.

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,71 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    prefix = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync');
+var gulp        = require('gulp'),
+    plumber     = require('gulp-plumber'),
+    sass        = require('gulp-ruby-sass'),
+    prefix      = require('gulp-autoprefixer'),
+    fileinclude = require('gulp-file-include'),
+    webserver   = require('gulp-webserver'),
+    opn         = require('opn');
 
 // Asset paths
 var paths = {
-  sass: ['_scss/**/*.scss'],
-  css:  ['css/**/*.css'],
-  html: ['*.html']
+  sass: '_scss/**/*.scss',
+  css:  'css',
+  templates: './templates/*.html',
+  build: './build'
 };
 
-// Proxy URL
-// If using with existing app, put the server url here
-// Also handy with Typekit and other services
-var devURL = "origin.dev";
+// Proxy URL (Handy for Typekit and other services)
+var server = {
+  host: 'origin.dev',
+  port: '8001'
+};
 
 
 // TASK: Compile Sass
 gulp.task('sass', function () {
   gulp.src(paths.sass)
+    .pipe(plumber())
     .pipe(sass({sourcemap: true, style: 'expanded', precision: '9'}))
     .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 9"))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest(paths.build + '/' + paths.css));
 });
 
 
-// TASK: BrowserSync
-gulp.task('browser-sync', function() {  
-  browserSync.init(paths.css, {
-    xip: true,
-    // open: false,
-    // notify: false,
+// TASK: Include files
+gulp.task('fileinclude', function() {
+  gulp.src(paths.templates)
+    .pipe(fileinclude())
+    .pipe(gulp.dest(paths.build));
+});
 
-    // Use this for vhost
-    // proxy: devURL,
-    // host: devURL
 
-    // Use this to ask Node to generate URL
-    server: {
-      baseDir: "./"
-    }
-  });
+// TASK: Set up a webserver
+gulp.task('webserver', function() {
+  gulp.src(paths.build)
+    .pipe(webserver({
+      host:             server.host,
+      port:             server.port,
+      livereload:       true
+    }));
+});
+
+
+// TASK: Open a browser window
+gulp.task('openbrowser', function() {
+  opn('http://' + server.host + ':' + server.port);
 });
 
 
 // TASK: Watch for changes
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.html, function(){
-    browserSync.reload();
-  });
+  gulp.watch([paths.templates], ['fileinclude']);
 });
 
 
+// TASK: Build project, no webserver
+gulp.task('build', ['sass', 'fileinclude']);
+
+
 // TASK: Default. Just run 'gulp' in CLI
-gulp.task('default', ['sass', 'browser-sync', 'watch']);
+gulp.task('default', ['build', 'webserver', 'watch', 'openbrowser']);
